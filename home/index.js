0a8523cbc494ab7432227e5cb1e41625d1440658
Lazuardi Hasan Kamil <lazuarditenyom@gmail.com>
2023-11-03 20:54:52 +0700

    js

---
+++ home/index.js
@@ -0,0 +1,9 @@
+let firstName = "John";
+let lastName = "Doe";
+let age = 25;
+let isMarried = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(typeof isMarried);
